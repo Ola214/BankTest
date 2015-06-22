7ad4708b3635b0c8cc2a178c462327228a035bbf
Aleksandra Krysińska <Ola214@interia.pl>
2015-06-22 22:25:39 +0200

    main changed etc.

--- client/src/main/java/com/ola/client/BankTestMain.java
+++ client/src/main/java/com/ola/client/BankTestMain.java
@@ -5,17 +5,71 @@
  */
 package com.ola.client;
 
+import com.ola.server.Account;
+import com.ola.server.Bank;
+import com.ola.server.BankImpl;
+import com.ola.server.User;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Olaa
  */
 public class BankTestMain {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Bank bank = new BankImpl();
+        try {
+            bank.getAccountState(2);
+        } catch (Bank.NoAccountFoundException ex) {
+            System.out.println("No account found");
+        }
+
+        User user = new User("ala", "makota", "a@gmail.com", bank.getId());
+        Account account = new Account(user.getId());
+        user.addAccount(account);
+        bank.addUser(user);
+
+        int counter = 0;
+        boolean called = false;
+        String surname = null;
+        while (counter < 3) {
+            if (called == false) {
+                try {
+                    System.out.println("Type surname");
+                    surname = getUserInput();
+                    bank.getAccountStatesOfUserAtSurname(surname);
+                    called = true;
+                } catch (Bank.NoUserFoundException ex) {
+                }
+            }
+            ++counter;
+        }
+
+        account.setAccountState(7.00);
+        called = false;
+        Double money = null;
+        while (called == false) {
+            try {
+                System.out.println("Type amount of money you want to withdraw");
+                money = Double.parseDouble(getUserInput());
+                bank.withdraw(account.getId(), money);
+                called = true;
+            } catch (Bank.DebetException de) {
+                
+            } catch (Bank.NegativeNumberException ex) {
+                System.exit(0);
+            }
+        }
+
     }
-    
+
+    public static String getUserInput() {
+        return scanner.nextLine();
+    }
+
 }
